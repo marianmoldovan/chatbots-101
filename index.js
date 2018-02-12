3ed52d22c1136444af364bf343ef2b8486edf662
beeva-marianmoldovan <marian.claudiu@beeva.com>
2018-02-12 10:55:50 +0100

    Improved way to use promise

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 const wit = new TelegrafWit(process.env.WIT_TOKEN)
 
 bot.on('text', (ctx) => {
-  return wit.meaning(ctx.message.text).then(respond).then(ctx.reply)
+  wit.meaning(ctx.message.text)
+    .then(nlu)
+    .then(ctx.reply)
 })
 
-function respond(nlu){
+function dialog(nlu){
   return new Promise((resolve, reject) => {
     if(nlu.entities.intent && nlu.entities.intent.length > 0){
       let intent = nlu.entities.intent[0]
       resolve(converser.smalltalk[intent.value])
     }
     else resolve('No te entiendorrrl. Pregúntame otra cosa fistro pecador.')
-  });
+  })
 }
 
 bot.startPolling()
